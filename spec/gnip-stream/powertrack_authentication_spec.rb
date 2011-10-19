77f6d951aff9395d01b57c5a28624c9a69ee9af2
Ryan Weald <ryan@weald.com>
2011-10-19 09:25:09 -0700

    make sure eventmachine stops when stubbing request. Also fix the format of the headers in the specs so it matches the new format returned from em-http-request 1.0

--- spec/gnip-stream/powertrack_authentication_spec.rb
+++ spec/gnip-stream/powertrack_authentication_spec.rb
@@ -10,6 +10,7 @@ describe GnipStream::PowertrackAuthentication do
       EM::HttpRequest.should_receive(:new).with("http://example.com").and_return(fake_request)
       fake_request.should_receive(:get) do |args|
         args[:head].should == {'authorization' => ["user", "password"]}
+        EM.stop
         double.as_null_object
       end
                                              
@@ -19,13 +20,14 @@ describe GnipStream::PowertrackAuthentication do
 
   describe "#parse_response_header" do
     it "should set the stream url based on 'LOCATION' header" do
-      subject.parse_response_header({"SET_COOKIE" => "session_token=foobar; domain=.gnip.com; path=/;
-          expires=Wed, 13-Jul-2011 22:10:10 GMT _base_session=hello; path=/; HttpOnly"})
+      subject.parse_response_header({"SET_COOKIE" => ["session_token=foobar; domain=.gnip.com; path=/;
+          expires=Wed, 13-Jul-2011 22:10:10 GMT _base_session=hello; path=/; HttpOnly"]})
       subject.cookies.should == "session_token=foobar; domain=.gnip.com; path=/"
     end
 
     it "should set the url that we can stream data from" do
-      subject.parse_response_header("LOCATION" => "http://example.com", "SET_COOKIE" => "")
+      subject.parse_response_header("LOCATION" => "http://example.com", "SET_COOKIE" => ["session_token=foobar; domain=.gnip.com; path=/;
+          expires=Wed, 13-Jul-2011 22:10:10 GMT _base_session=hello; path=/; HttpOnly"])
       subject.location.should == "http://example.com"
     end
   end
