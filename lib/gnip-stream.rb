da36d1e44e97dfcf223a74ba8ca201f223774ab9
Ryan Weald <ryan@weald.com>
2011-10-18 20:40:52 -0700

    require all the independent pieces if user requires top level file.

--- lib/gnip-stream.rb
+++ lib/gnip-stream.rb
@@ -1,6 +1,11 @@
 require "gnip-stream/version"
 
+require 'gnip-stream/stream'
+require 'gnip-stream/data_buffer'
 require 'gnip-stream/json_stream'
+require 'gnip-stream/xml_stream'
+require 'gnip-stream/powertrack_client'
+require 'gnip-stream/facebook_client'
 
 module GnipStream
 end
