6dbaa30c9f9eae8351eab5ab4dc59162b8a72718
Ryan Weald <ryan@weald.com>
2011-10-18 20:26:45 -0700

    Add simple PowertrackClient as way to interact with powertrack api. This is a high level object that simply delegates to contained objects.

---
+++ lib/gnip-stream/powertrack_client.rb
@@ -0,0 +1,26 @@
+module GnipStream
+  class PowertrackClient
+    def initialize(url, username, password)
+      @authentication = PowertrackAuthentication.new(url, username, password)
+      @stream = JsonStream.new(url)
+      configure_handlers
+    end
+
+    def configure_handlers
+      @stream.on_error { |error| raise "Gnip Connection Error. Reason was: #{error}" }
+      @stream.on_connection_close { raise "Gnip Connection Closed" }
+    end
+
+    def consume(&block)
+      @stream.on_message(&block)
+      authenticate
+      @stream.connect
+    end
+
+    def authenticate
+      @authentication.authenticate
+      @stream.url = @authentication.url
+      @stream.headers = {"cookie" => @authentication.cookies}
+    end
+  end
+end
