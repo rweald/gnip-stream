887d8ad6d5edc385d38a856359e20ae17ce6dcb1
Ryan Weald <ryan@weald.com>
2011-10-19 10:28:15 -0700

    use ErrorReconnect objects to handle errors in our client classes. This delegation means these classes don't have to deal with reconnect logic

--- lib/gnip-stream/facebook_client.rb
+++ lib/gnip-stream/facebook_client.rb
@@ -4,12 +4,14 @@ module GnipStream
   class FacebookClient
     def initialize(url, username, password)
       @stream = XmlStream.new(url, "authorization" => [username, password])
+      @error_handler = ErrorReconnect.new(self, :consume)
+      @connection_close_handler = ErrorReconnect.new(self, :consume)
       configure_handlers
     end
 
     def configure_handlers
-      @stream.on_error { |error| raise "Gnip Connection Error. Reason was: #{error}" }
-      @stream.on_connection_close { raise "Gnip Connection Closed" }
+      @stream.on_error { |error| @error_handler.attempt_to_reconnect("Gnip Connection Error. Reason was: #{error.inspect}") }
+      @stream.on_connection_close { @connection_close_handler.attempt_to_reconnect("Gnip Connection Closed") }
     end
 
     def consume(&block)
--- lib/gnip-stream/powertrack_client.rb
+++ lib/gnip-stream/powertrack_client.rb
@@ -4,12 +4,14 @@ module GnipStream
     def initialize(url, username, password)
       @authentication = PowertrackAuthentication.new(url, username, password)
       @stream = JsonStream.new(url)
+      @error_handler = ErrorReconnect.new(self, :consume)
+      @connection_close_handler = ErrorReconnect.new(self, :consume)
       configure_handlers
     end
 
     def configure_handlers
-      @stream.on_error { |error| raise "Gnip Connection Error. Reason was: #{error.inspect}" }
-      @stream.on_connection_close { raise "Gnip Connection Closed" }
+      @stream.on_error { |error| @error_handler.attempt_to_reconnect("Gnip Connection Error. Reason was: #{error.inspect}") }
+      @stream.on_connection_close { @connection_close_handler.attempt_to_reconnect("Gnip Connection Closed") }
     end
 
     def consume(&block)
