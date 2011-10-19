4fbb11fed01711e30d22ab7bc8904f9c2ad31560
Ryan Weald <ryan@weald.com>
2011-10-18 17:46:25 -0700

    create basic JsonStream class. This class simply delegates to Stream using a json data buffer.

--- spec/gnip-stream/json_stream_spec.rb
+++ spec/gnip-stream/json_stream_spec.rb
@@ -2,4 +2,30 @@ require 'spec_helper'
 require 'gnip-stream/json_stream'
 
 describe GnipStream::JsonStream do
+  subject { GnipStream::JsonStream.new("http://example.com") }
+
+  describe "#initialize" do
+    it "creates underlying stream object with a json specific data buffer" do
+      GnipStream::Stream.should_receive(:new) do |url|
+        url.should == "http://example.com"
+      end
+      GnipStream::JsonStream.new("http://example.com")
+    end
+  end
+
+  describe "#method_missing" do
+    let(:underlying_stream) { double("GnipStream::Stream") }
+    before do
+      GnipStream::Stream.stub(:new => underlying_stream)
+    end
+
+    it "delegates all available methods to the underlying stream class" do
+      underlying_stream.should_receive(:connect)
+      subject.connect
+    end
+
+    it "raises a method not found error on self if underlying stream can not respond to the method" do
+      lambda { subject.foobar }.should raise_error(NoMethodError)
+    end
+  end
 end
