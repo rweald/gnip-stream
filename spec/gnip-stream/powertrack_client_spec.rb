4eead50c54d6645f054b2e1ef80f2bbac193de80
Ryan Weald <ryan@weald.com>
2012-02-21 20:18:01 -0800

    Remove external authentication from powertrack.
    GNIP changed there powertrack API recently and no longer requires
    the redirect based authentication. Therefore you can just use
    http basic auth

--- spec/gnip-stream/powertrack_client_spec.rb
+++ spec/gnip-stream/powertrack_client_spec.rb
@@ -3,22 +3,19 @@ require 'gnip-stream/powertrack_client'
 
 describe GnipStream::PowertrackClient do
   let(:fake_stream) { double("GnipStream::JsonStream").as_null_object }
-  let(:fake_auth) { double("GnipStream::PowertrackAuthentication").as_null_object }
   before do
     GnipStream::JsonStream.stub(:new => fake_stream)
-    GnipStream::PowertrackAuthentication.stub(:new => fake_auth) 
   end
 
   subject { GnipStream::PowertrackClient.new("http://example.com", "user", "password") }
 
   describe "#initialize" do
-    it "initializes a PowertrackAuthentication instance" do
-      GnipStream::PowertrackAuthentication.should_receive(:new)
-      subject
-    end
-
     it "initializes an instance JsonStream" do
-      GnipStream::JsonStream.should_receive(:new) 
+      GnipStream::JsonStream.should_receive(:new) do |url, headers|
+        url.should == "http://example.com"
+        headers["authorization"].should == ["user", "password"]
+        headers["accept-encoding"].should == "gzip, compressed"
+      end
       subject
     end
   end
@@ -28,18 +25,6 @@ describe GnipStream::PowertrackClient do
       fake_stream.should_receive(:on_error).twice    
       fake_stream.should_receive(:on_connection_close).twice
       subject.configure_handlers
-    end
-  end
-
-  describe "#authenticate" do
-    it "performs the necessary authentication and passes appropriate credentials to stream" do
-      fake_auth.should_receive(:authenticate)
-      fake_auth.should_receive(:location).and_return("http://example.com")
-      fake_auth.should_receive(:cookies).and_return("some cookie")
-
-      fake_stream.should_receive(:url=).with("http://example.com") 
-      fake_stream.should_receive(:headers=)
-      subject.authenticate
     end
   end
 
