3c0604cf5d52b692c2802ad627f9a60f67cf713e
Yian Shang <yian.shang@gmail.com>
2016-01-05 13:15:35 -0800

    Adding logging

--- lib/gnip-stream/error_reconnect.rb
+++ lib/gnip-stream/error_reconnect.rb
@@ -7,12 +7,15 @@ module GnipStream
     end
 
     def attempt_to_reconnect(error_message)
+      puts error_message
       @error_message = error_message
       if @reconnect_attempts < 5
+        puts "Reconnecting..."
         @reconnect_attempts +=1
         sleep(2)
         @source_class.send(@method_name)
       else
+        puts "Reconnecting failed."
         reconnect_failed_raise_error
       end
     end
